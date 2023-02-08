6de8ac431fdcf8a75e8e8a29cb3f5166b4ba0086
Sarwar Zahan <szmithu1@gmail.com>
2023-02-08 12:47:19 +0600

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Billing from './Billing';
+import Business from './Business';
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+import Stats from './Stats';
+import Footer from './Footer';
+import Testimonials from './Testimonials';
+
+export {
+     Navbar,
+     Hero,
+     Billing,
+     Business,
+     CardDeal,
+     Clients,
+     CTA,
+     Stats,
+     Footer,
+     Testimonials,
+}
